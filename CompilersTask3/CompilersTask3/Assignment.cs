70dc678a4f5de1cfa5a5b89c4806b040b6b62c8a
Roberto Chavez <rchavez.intertec@gmail.com>
2019-10-11 17:11:52 +0300

    First Update

--- CompilersTask3/CompilersTask3/Assignment.cs
+++ CompilersTask3/CompilersTask3/Assignment.cs
@@ -1,7 +1,11 @@
-﻿namespace CompilersTask3
+﻿using System.Linq.Expressions;
+
+namespace CompilersTask3
 {
     public class Assignment
     {
         private LeftPart _leftPart;
+        private Operator _operator;
+        private Expression _expression;
     }
 }
---
+++ CompilersTask3/CompilersTask3/Expression.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CompilersTask3
+{
+    public class Expression
+    {
+        private List<Term> _terms;
+        private AddSign _addSign;
+    }
+}
